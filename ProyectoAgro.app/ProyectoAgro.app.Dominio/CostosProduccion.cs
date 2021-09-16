687120dff880d5a0cf56cc17fb0cf1962d5d37d2
LauD12-oss <90513868+LauD12-oss@users.noreply.github.com>
2021-09-15 20:33:25 -0500

    Se agrego edad

--- ProyectoAgro.app/ProyectoAgro.app.Dominio/CostosProduccion.cs
+++ ProyectoAgro.app/ProyectoAgro.app.Dominio/CostosProduccion.cs
@@ -9,6 +9,7 @@ namespace Dominio
         public int Ganancia {get;set;}
         public int Salario {get;set;}
         public string nombre {get;set;}
+        public int Edad {get;set;}
 
     }
 }
