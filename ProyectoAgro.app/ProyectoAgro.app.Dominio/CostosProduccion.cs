6c9824aa7a547196ce428279438a149df14654fb
LauD12-oss <90513868+LauD12-oss@users.noreply.github.com>
2021-09-16 08:00:27 -0500

    Update CostosProduccion.cs

--- ProyectoAgro.app/ProyectoAgro.app.Dominio/CostosProduccion.cs
+++ ProyectoAgro.app/ProyectoAgro.app.Dominio/CostosProduccion.cs
@@ -7,9 +7,7 @@ namespace Dominio
         public int Inversion {get;set;}
         public int Gasto {get;set;}
         public int Ganancia {get;set;}
-        public int Salario {get;set;}
-        public string nombre {get;set;}
-        public int Edad {get;set;}
+        
 
     }
 }
